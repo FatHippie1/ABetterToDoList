3e17afb9b1c6244eeeedbb25443c3c15fc6ec208
FatHippie1 <80604504+FatHippie1@users.noreply.github.com>
2022-09-17 18:13:57 -0600

    attempting to line through

--- scripts.js
+++ scripts.js
@@ -4,32 +4,60 @@ const submitBtn = document.getElementById('submitBtn');
 const tableItem = document.querySelector('table');
 const remove = document.getElementById('dlt');
 const todos = document.getElementsByClassName('td');
+const checkedBox = document.getElementById('checkbox');
 
 
-submitBtn.addEventListener('click', submitTodo);
-remove.addEventListener('click', removeTodo);
+submitBtn.addEventListener('click', createTodo);
+//remove.addEventListener('click', removeTodo);
 
-function submitTodo() {
-    tableItem.innerHTML += `<td class='td'>${toDo.value}</td><td><input type='checkbox'></td><td><button id="dlt2" type='button'>Delete</button></td>`;
-
+function createTodo() {
+    tableItem.innerHTML += `
+    <td class='td'id="createdTodo">
+    ${toDo.value}
+    </td>
+    <td>
+    <input type='checkbox' id="checkbox" onclick="completeTodo(event)">
+    </td>
+    <td>
+    <button id="dlt2" onclick="deleteToDoItem(event)" type='button'>Delete</button>
+    </td>`;
 }
 
+function deleteToDoItem(e) {
+    console.log('clicked button');
+    const button = e.target;
+    const parentTD = button.parentElement;
+    const parentTR = parentTD.parentElement;
+    parentTR.remove();
+    //e.target.remove();
+}
 
+//checkedBox.addEventListener('checked',completeTodo)
+function completeTodo(e) {
+    console.log('got it');
+    
+    const checkBox = e.target;
+    const completedTodo = checkBox.parentElement;
+    const actualCompletedTodo = completedTodo.previousElementSibling;
+    console.log(actualCompletedTodo.style.textDecoration);
+    completedTodo.style.textDecoration = 'line-through';
+    //e.target.style.textDecoration = 'line-through';
+    
+    }
 
-function removeTodo() {
-
-    console.log("clicked");
+//function removeTodo() {
+    
+    // console.log("clicked");
     //console.log(tableItem)
-    //tableItem.remove();
+//tableItem.remove();
 
-}
+//}
 
-const items = document.querySelectorAll('td');
+//const items = document.querySelectorAll('td');
 
-items.forEach(item => {
-    item.addEventListener('click', e => {
-        //e.target.style.textDecoration = 'line-through';
-        e.target.remove();
-    });
-});
+//items.forEach(item => {
+   // item.addEventListener('click', e => {
+       // e.target.remove();
+   // });
+//});
 //<td><input type='checkbox></td>
