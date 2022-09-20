ea52d18767dc0d0845ddfff46d7428c8657bc3dc
FatHippie1 <80604504+FatHippie1@users.noreply.github.com>
2022-09-20 01:45:04 -0600

    line through working

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ const checkedBox = document.getElementById('checkbox');
 
 
 submitBtn.addEventListener('click', createTodo);
-//remove.addEventListener('click', removeTodo);
+
 
 function createTodo() {
     tableItem.innerHTML += `
@@ -16,71 +16,57 @@ function createTodo() {
     ${toDo.value}
     </td>
     <td>
-    <input type='checkbox' id="checkbox" onclick="isChecked()">
+    <input type='checkbox' id="checkbox" onclick="isChecked(event)">
     </td>
     <td>
     <button id="dlt2" onclick="deleteToDoItem(event)" type='button'>Delete</button>
     </td>`;
+
+
 }
 
+
 function deleteToDoItem(e) {
-    console.log('clicked button');
     const button = e.target;
     const parentTD = button.parentElement;
     const parentTR = parentTD.parentElement;
+
     parentTR.remove();
-    //e.target.remove();
+
+
 }
 
-//checkedBox.addEventListener('checked',completeTodo)
+function isChecked(e) {
+    const checkbox = e.target;
+    const parentTR = checkbox.parentElement;
+    const siblingTD = parentTR.previousElementSibling;
 
-function isChecked() {
-    const checkbox2 = document.querySelector("#createdTodo");
-    //const parentTD = checkbox.parentElement;
-    //const sibling = parentTD.previousElementSibling;
-    if (checkbox.checked){
-    checkbox2.style.textDecoration = 'line-through';
+    if (checkbox.checked) {
+        siblingTD.style.textDecoration = 'line-through';
     }
     else {
-        checkbox2.style.textDecoration = '';
+        siblingTD.style.textDecoration = '';
     }
-    console.log(checkbox.checked);
+    return isChecked(checkbox);
+};
+
+//function deleteCompleted(checkbox) {
+   //let c = checkbox;
+    //if (checkbox) {
+        //console.log('yes');
+    //}
+    //deleteToDoItem();
+    //isChecked();
+    //const a = checkbox;
+    //const b = button;
+    //if (checkbox && button) {
+    //console.log(a, 'got checkblx');
+    //parentTR.remove();
+
+    //deleteToDoItem();
+    //}
+
+//};
+//deleteCompleted();
 
 
-/*    const chckbx = document.getElementById('checkbox');
-    const toDo = document.getElementById('createdTodo');
-
-    if (chckbx.checked) {
-        toDo.previousSiblingElement.setAttribute.textDecoration='line-through'
-    }
-    else {
-        toDo.setAttribute('textDecoration','')
-    }
-  */
-    /*console.log('got it');
-    
-    const checkBox = e.target;
-    const completedTodo = checkBox.parentElement;
-    const actualCompletedTodo = completedTodo.previousElementSibling;
-    console.log(actualCompletedTodo.style.textDecoration);
-    completedTodo.style.textDecoration = 'line-through';
-    //e.target.style.textDecoration = 'line-through';
-    */
-    }
-
-//function removeTodo() {
-    
-    // console.log("clicked");
-    //console.log(tableItem)
-//tableItem.remove();
-
-//}
-
-//const items = document.querySelectorAll('td');
-
-//items.forEach(item => {
-   // item.addEventListener('click', e => {
-       // e.target.remove();
-   // });
-//});
-//<td><input type='checkbox></td>
